5e4ed9ab456103b772af0daf0f8d1e2b6233be0f
Niarana <narana@kaplan.edu>
2019-01-28 10:06:45 -0500

    Add support for ng7.

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule }   from '@angular/forms';
 import { AutoCompleteComponent } from './autocomplete.component';
 import { BoldPrefix } from './boldprefix.pipe';
-import {IonicModule} from 'ionic-angular';
+import {IonicModule} from '@ionic/angular';
 
 export * from './autocomplete.component';
 export * from './boldprefix.pipe';
